3b083ed71d68bd5ba0e72ebffc8a1da14bef7591
jacobka2la <162231046+jacobka2la@users.noreply.github.com>
2026-08-14 12:21:41 -0400

    Keep client and folder pages scrolled to top on navigation

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,12 +1,33 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    const scrollToTop = () => {
+      window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    };
+
+    const originalPushState = window.history.pushState.bind(window.history);
+    const originalReplaceState = window.history.replaceState.bind(window.history);
+
+    window.history.pushState = (...args) => {
+      originalPushState(...args);
+      scrollToTop();
+    };
+
+    window.history.replaceState = (...args) => {
+      originalReplaceState(...args);
+      scrollToTop();
+    };
+
+    window.addEventListener("popstate", scrollToTop);
+    scrollToTop();
+
+    return () => {
+      window.history.pushState = originalPushState;
+      window.history.replaceState = originalReplaceState;
+      window.removeEventListener("popstate", scrollToTop);
+    };
+  }, []);
 
   return null;
 };
