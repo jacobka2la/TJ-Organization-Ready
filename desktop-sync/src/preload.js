1a5f77d6287e97c6aa7c80db77e49fe70e9e0e43
jacobka2la <162231046+jacobka2la@users.noreply.github.com>
2026-08-18 10:33:44 -0400

    Add background window bridge

--- desktop-sync/src/preload.js
+++ desktop-sync/src/preload.js
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('tjDesktop', {
   login: (email, password) => ipcRenderer.invoke('tj-login', { email, password }),
   openFolder: () => ipcRenderer.invoke('tj-open-folder'),
+  hide: () => ipcRenderer.invoke('tj-hide'),
 });
