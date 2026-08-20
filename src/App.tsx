8fdf38e6cdc8a9aaba7412f8117e9f1f7cf07756
jacobka2la <162231046+jacobka2la@users.noreply.github.com>
2026-08-20 12:27:15 -0400

    Remove unnecessary react-router dependency from app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 
@@ -9,13 +8,8 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <ScrollToTop />
+      <Home />
     </TooltipProvider>
   </QueryClientProvider>
 );
